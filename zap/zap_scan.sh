cdfd1e4a61e65310a802c049e2d6d32aec621c47
Garvit <garvit@wednesday.is>
2024-08-21 17:58:34 +0530

    Update docker run command

--- zap/zap_scan.sh
+++ zap/zap_scan.sh
@@ -5,10 +5,10 @@ docker run -u root --name zap -p 8080:8080 -d zaproxy/zap-stable zap.sh -daemon 
 sleep 20
 
 # Run a ZAP scan against your deployed app on CloudFront
-docker run -u zap -v $(pwd)/zap:/zap/wrk/:rw --network="host" zaproxy/zap-stable zap-cli quick-scan --self-contained --start-options '-config api.disablekey=true' -r scan_report.html https://zaproxy.org/
+docker run -u root -v $(pwd)/zap:/zap/wrk/:rw --network="host" zaproxy/zap-stable zap-full-scan.py --self-contained --start-options '-config api.disablekey=true' -r scan_report.html -t https://zaproxy.org/
 
 # Copy the report
-docker cp $(docker ps -q -f "ancestor=owasp/zap2docker-stable"):/zap/wrk/scan_report.html .
+# docker cp $(docker ps -q -f "ancestor=owasp/zap2docker-stable"):/zap/wrk/scan_report.html .
 
 # Optionally fail the build if the scan finds critical vulnerabilities
-docker run -u zap -v $(pwd)/zap:/zap/wrk/:rw --network="host" zaproxy/zap-stable zap-cli quick-scan --self-contained --start-options '-config api.disablekey=true' -r scan_report.html https://zaproxy.org/ --exit-code False
+# docker run -u root -v $(pwd)/zap:/zap/wrk/:rw --network="host" zaproxy/zap-stable zap-full-scan.py --self-contained --start-options '-config api.disablekey=true' -r scan_report.html -t https://zaproxy.org/ --exit-code False
